82d4617974c91b5c12a35c3321b00f214b354737
Sam Cappleman-Lynes <Sam.Cappleman-Lynes@softwire.com>
2017-10-06 13:40:54 +0100

    Add a test for pawns moving two spaces initially

--- src/engine/board.ts
+++ src/engine/board.ts
@@ -7,8 +7,8 @@ export default class Board {
     public currentPlayer: Player;
     private readonly board: (Piece | undefined)[][];
 
-    public constructor() {
-        this.currentPlayer = Player.WHITE;
+    public constructor(currentPlayer) {
+        this.currentPlayer = currentPlayer ? currentPlayer : Player.WHITE;
         this.board = this.createBoard();
     }
 
