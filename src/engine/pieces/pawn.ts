7fa0aab343f819e1363a4f6713444a87acd55cd7
Stefan Ghenescu <Stefan.Ghenescu@softwire.com>
2025-07-07 16:12:15 +0300

    Verify that there is not a piece in front

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -17,19 +17,26 @@ export default class Pawn extends Piece {
         // verify whose turn is it
         if (this.player == Player.WHITE) {
             // first move can be of 2 squares
-            if (board.whiteFirstMove)
+            if (board.whiteFirstMove) {
                 twoSquareNext = new Square(currentSquare.row + 2, currentSquare.col);
+            }
             oneSquareNext = new Square(currentSquare.row + 1, currentSquare.col);
         } else {
             // first move can be of 2 squares
-            if (board.blackFirstMove)
+            if (board.blackFirstMove) {
                 twoSquareNext = new Square(currentSquare.row - 2, currentSquare.col);
+            }
             oneSquareNext = new Square(currentSquare.row - 1, currentSquare.col);
         }
 
-        availableMoves.push(oneSquareNext);
-        if (twoSquareNext)
-            availableMoves.push(twoSquareNext);
+        // verify that there is no other piece there
+        if (board.getPiece(oneSquareNext) == null) {
+            availableMoves.push(oneSquareNext);
+
+            // verify that both squares are free as a pawn cannot jump over a piece
+            if (twoSquareNext && board.getPiece(twoSquareNext) == null)
+                availableMoves.push(twoSquareNext);
+        }
 
         return availableMoves;
     }
